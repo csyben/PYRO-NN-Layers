8c705afa4a8d0dd998ef6faf9bad81a9bf279f82
syben <syben>
2018-04-12 21:10:15 +0200

    setting up lme_custom_ops kernels to lme_custom_ops namepsace

--- cuda_operator.py
+++ cuda_operator.py
@@ -3,11 +3,9 @@ import tensorflow as tf
 import lme_custom_ops
 
 
-print("HELLO from lme_custom_op operator")
-
 if tf.test.is_built_with_cuda():
     _lme_custom_ops_module = tf.load_op_library(os.path.join(
         tf.resource_loader.get_data_files_path(), 'lme_custom_ops.so'))
-
+    ''' TODO: Improve the getattr method to add only real kernel methods and not everything '''
     for obj in dir(_lme_custom_ops_module):
         setattr(lme_custom_ops, obj, getattr(_lme_custom_ops_module, obj))
