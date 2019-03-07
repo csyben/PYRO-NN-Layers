7d13e9f33948143e462c17600f20e2144a4929a3
syben <christopher.syben@fau.de>
2019-03-07 14:54:49 +0100

    added methods for computation of camercenter, right nullspace and line intersections.

---
+++ helper_headers/helper_geometry_cpu.h
@@ -0,0 +1,27 @@
+
+#ifndef HELPER_GEOMETRY_CPU_H
+#define HELPER_GEOMETRY_CPU_H
+#pragma once
+//#include "SingularValueDecomposition.h"
+#include <Eigen/Dense>
+#include <Eigen/SVD>
+
+namespace Geometry{
+    
+    /// Compute right null-space of A
+    Eigen::VectorXf nullspace(const Eigen::MatrixXf& A)
+    {
+        Eigen::JacobiSVD<Eigen::MatrixXf> svd(A, Eigen::ComputeFullU | Eigen::ComputeFullV);
+        auto V=svd.matrixV();
+        return V.col(V.cols()-1);
+    }
+    /// Extract the world coordinates of the camera center from a projection matrix. (SVD based implementation)
+    Eigen::Vector4f getCameraCenter(const Eigen::MatrixXf& P)
+    {
+        Eigen::Vector4f C = Geometry::nullspace(P);
+        if (C(3)<-1e-12 || C(3)>1e-12)
+            C=C/C(3); // Def:Camera centers are always positive.
+        return C;
+    }
+}
+#endif
