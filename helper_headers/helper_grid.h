dc4b474c724ed1b9867fe488df2bd445ea669311
syben <christopher.syben@fau.de>
2019-03-07 19:22:47 +0100

    moved 3D version of i2p and p2i to master branch

--- helper_headers/helper_grid.h
+++ helper_headers/helper_grid.h
@@ -44,6 +44,16 @@ inline __host__ __device__ float2 physical_to_index(float2 physical, float2 orig
     return make_float2((physical.x - origin.x) / spacing.x, (physical.y - origin.y) / spacing.y);
 }
 
+inline __host__ __device__ float3 index_to_physical(float3 index, float3 origin, float3 spacing)
+{
+    return make_float3(index.x * spacing.x + origin.x, index.y * spacing.y + origin.y, index.z * spacing.z + origin.z);
+}
+
+inline __host__ __device__ float3 physical_to_index(float3 physical, float3 origin, float3 spacing)
+{
+    return make_float3((physical.x - origin.x) / spacing.x, (physical.y - origin.y) / spacing.y, (physical.z - origin.z) / spacing.z);
+}
+
 #endif
 
 
