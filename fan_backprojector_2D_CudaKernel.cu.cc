a96e34800be45950513f36e6d7aef07cc90d207e
syben <christopher.syben@fau.de>
2019-04-29 17:15:55 +0200

    added Copyright and Apache 2.0 license information to every file.

--- fan_backprojector_2D_CudaKernel.cu.cc
+++ fan_backprojector_2D_CudaKernel.cu.cc
@@ -1,3 +1,21 @@
+/*
+ * Copyright [2019] [Christopher Syben]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Voxel-driven fan-beam back-projection CUDA kernel.
+ * PYRO-NN is developed as an Open Source project under the Apache License, Version 2.0.
+*/
 #if GOOGLE_CUDA
 #define EIGEN_USE_GPU
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
@@ -103,9 +121,3 @@ void Fan_Backprojection2D_Kernel_Launcher(const float *sinogram_ptr, float *out,
 }
 
 #endif
-
-
-/*
- * Voxel-driven fan-beam back-projection CUDA kernel
- * PYRO-NN is developed as an Open Source project under the GNU General Public License (GPL).
-*/
