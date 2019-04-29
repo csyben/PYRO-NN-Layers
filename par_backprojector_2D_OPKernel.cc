3d23a8ca1003aef84e8fd6710c903b5b800cd706
syben <christopher.syben@fau.de>
2019-04-29 17:16:07 +0200

    added Copyright and Apache 2.0 license information to every file.

--- par_backprojector_2D_OPKernel.cc
+++ par_backprojector_2D_OPKernel.cc
@@ -1,3 +1,21 @@
+/*
+ * Copyright [2019] [Christopher Syben]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * Links the parallel-beam back-projector layer from python to the actual kernel implementation. Implemented according to Tensorflow API.
+ * PYRO-NN is developed as an Open Source project under the Apache License, Version 2.0.
+*/
 #include "tensorflow/core/framework/op.h"
 #include "tensorflow/core/framework/op_kernel.h"
 
@@ -109,9 +127,3 @@ class ParallelBackprojection2DOp : public OpKernel
 };
 
 REGISTER_KERNEL_BUILDER(Name(CUDA_OPERATOR_KERNEL).Device(DEVICE_GPU), ParallelBackprojection2DOp);
-
-
-/*
- * Links the parallel-beam back-projector layer from python to the actual kernel implementation. Implemented according to Tensorflow API.
- * PYRO-NN is developed as an Open Source project under the GNU General Public License (GPL).
-*/
