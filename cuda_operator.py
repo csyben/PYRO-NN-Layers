65515d195cae7582f1730194f0b628fd1f6f07a7
syben <christopher.syben@fau.de>
2019-04-29 17:21:09 +0200

    added Copyright and Apache 2.0 license information to every file.

--- cuda_operator.py
+++ cuda_operator.py
@@ -1,3 +1,20 @@
+# Copyright [2019] [Christopher Syben]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Makes every implemented operator in python available under the namespace pyronn_layers
+# PYRO-NN is developed as an Open Source project under the Apache License, Version 2.0.
+#
 import os.path
 import tensorflow as tf
 import pyronn_layers
@@ -11,8 +28,3 @@ if tf.test.is_built_with_cuda():
         setattr(pyronn_layers, obj, getattr(_pyronn_layers_module, obj))
 
 
-
-#
- # Makes every implemented operator in python available under the namespace pyronn_layers
- # PYRO-NN is developed as an Open Source project under the GNU General Public License (GPL).
-#
