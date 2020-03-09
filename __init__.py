829463374af3c3920f721efa3b704b091536d851
syben <christopher.syben@fau.de>
2020-03-09 13:01:42 +0100

    add __init__ file according to tensorflows docker

--- __init__.py
+++ __init__.py
@@ -1,1 +1,21 @@
-import pyronn_layers.cuda_operator
+# Copyright [2019] [Christopher Syben]
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Makes every implemented operator in python available under the namespace pyronn_layers
+# PYRO-NN is developed as an Open Source project under the Apache License, Version 2.0.
+#
+
+from __future__ import absolute_import
+
+from pyronn_layers.python.ops.pyronn_layers_ops import pyronn_layers
