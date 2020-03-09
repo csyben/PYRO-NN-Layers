9ae900d5c070e0466e905f5aa51da0ec6f209915
syben <christopher.syben@fau.de>
2020-03-09 13:09:31 +0100

    Reworked Layer API. Removed attributes, refactored to input tensors to be compatible with eager excecution. Added batch support.

--- cc/kernels/par_backprojector_2D_CudaKernel.cu.cc
+++ cc/kernels/par_backprojector_2D_CudaKernel.cu.cc
@@ -20,15 +20,15 @@
 #if GOOGLE_CUDA
 #define EIGEN_USE_GPU
 #include "third_party/eigen3/unsupported/Eigen/CXX11/Tensor"
-#include "helper_headers/helper_grid.h"
-#include "helper_headers/helper_math.h"
+#include "../helper_headers/helper_grid.h"
+#include "../helper_headers/helper_math.h"
 
 texture<float, cudaTextureType2D, cudaReadModeElementType> sinogram_as_texture;
 #define CUDART_INF_F __int_as_float(0x7f800000)
 
 __global__ void backproject_2Dpar_beam_kernel(float *pVolume, const float2 *d_rays, const int number_of_projections,
-                                              const int2 volume_size, const float2 volume_spacing, const float2 volume_origin,
-                                              const int detector_size, const float detector_spacing, const float detector_origin)
+                                              const int2 volume_size, const float *volume_spacing_ptr, const float *volume_origin_ptr,
+                                              const int detector_size, const float *detector_spacing, const float *detector_origin)
 {
     const float pi = 3.14159265359f;
     unsigned int volume_x = blockIdx.x * blockDim.x + threadIdx.x;
@@ -38,6 +38,9 @@ __global__ void backproject_2Dpar_beam_kernel(float *pVolume, const float2 *d_ra
         return;
     }
     //Preparations:
+    //Prep: Wrap pointer to float2 for better readable code
+    float2 volume_spacing = make_float2(*(volume_spacing_ptr+1), *volume_spacing_ptr);
+    float2 volume_origin = make_float2(*(volume_origin_ptr+1), *volume_origin_ptr);
     const float2 pixel_coordinate = index_to_physical(make_float2(volume_x, volume_y), volume_origin, volume_spacing);
     float pixel_value = 0.0f;
 
@@ -47,7 +50,7 @@ __global__ void backproject_2Dpar_beam_kernel(float *pVolume, const float2 *d_ra
         float2 detector_vec = make_float2(-detector_normal.y, detector_normal.x);
 
         float s = dot(pixel_coordinate, detector_vec);
-        float s_idx = physical_to_index(s, detector_origin, detector_spacing);
+        float s_idx = physical_to_index(s, *detector_origin, *detector_spacing);
 
         pixel_value += tex2D(sinogram_as_texture, s_idx + 0.5f, n + 0.5f);
     }
@@ -73,9 +76,9 @@ __global__ void backproject_2Dpar_beam_kernel(float *pVolume, const float2 *d_ra
     * 
     */
 void Parallel_Backprojection2D_Kernel_Launcher(const float *sinogram_ptr, float *out, const float *ray_vectors, const int number_of_projections,
-                                               const int volume_width, const int volume_height, const float volume_spacing_x, const float volume_spacing_y,
-                                               const float volume_origin_x, const float volume_origin_y,
-                                               const int detector_size, const float detector_spacing, const float detector_origin)
+                                               const int volume_size_x, const int volume_size_y,
+                                               const float *volume_spacing_ptr, const float *volume_origin_ptr,
+                                               const int detector_size, const float *detector_spacing_ptr, const float *detector_origin_ptr)
 {
     cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc<float>();
     sinogram_as_texture.addressMode[0] = cudaAddressModeBorder;
@@ -88,26 +91,18 @@ void Parallel_Backprojection2D_Kernel_Launcher(const float *sinogram_ptr, float 
     cudaMemcpyToArray(sinogram_array, 0, 0, sinogram_ptr, detector_size * number_of_projections * sizeof(float), cudaMemcpyHostToDevice);
     cudaBindTextureToArray(sinogram_as_texture, sinogram_array, channelDesc);
 
-    auto ray_size_b = number_of_projections * sizeof(float2);
-    float2 *d_rays;
-    cudaMalloc(&d_rays, ray_size_b);
-    cudaMemcpy(d_rays, ray_vectors, ray_size_b, cudaMemcpyHostToDevice);
-
-    int2 volume_size = make_int2(volume_width, volume_height);
-    float2 volume_spacing = make_float2(volume_spacing_x, volume_spacing_y);
-    float2 volume_origin = make_float2(volume_origin_x, volume_origin_y);
+    int2 volume_size = make_int2(volume_size_x, volume_size_y);
 
     const unsigned block_size = 16;
     const dim3 threads_per_block = dim3(block_size, block_size);
-    const dim3 num_blocks = dim3(volume_width / threads_per_block.x + 1, volume_height / threads_per_block.y + 1);
+    const dim3 num_blocks = dim3(volume_size_x / threads_per_block.x + 1, volume_size_y / threads_per_block.y + 1);
 
-    backproject_2Dpar_beam_kernel<<<num_blocks, threads_per_block>>>(out, d_rays, number_of_projections,
-                                                                     volume_size, volume_spacing, volume_origin,
-                                                                     detector_size, detector_spacing, detector_origin);
+    backproject_2Dpar_beam_kernel<<<num_blocks, threads_per_block>>>(out, ((float2 *) ray_vectors), number_of_projections,
+                                                                     volume_size, volume_spacing_ptr, volume_origin_ptr,
+                                                                     detector_size, detector_spacing_ptr, detector_origin_ptr);
 
     cudaUnbindTexture(sinogram_as_texture);
     cudaFreeArray(sinogram_array);
-    cudaFree(d_rays);
 }
 
 #endif
--- cc/ops/par_backprojector_2D_OPKernel.cc
+++ cc/ops/par_backprojector_2D_OPKernel.cc
@@ -20,32 +20,22 @@
 #include "tensorflow/core/framework/op_kernel.h"
 #include "tensorflow/core/framework/shape_inference.h"
 using namespace tensorflow; // NOLINT(build/namespaces)
-using shape_inference::ShapeHandle; 
 
 #define CUDA_OPERATOR_KERNEL "ParallelBackprojection2D"
 
 REGISTER_OP(CUDA_OPERATOR_KERNEL)
     .Input("sinogram: float")
-    .Attr("sinogram_shape: shape")
-    .Attr("volume_shape: shape")
-    .Attr("volume_origin : tensor")
-    .Attr("detector_origin : tensor")
-    .Attr("volume_spacing : tensor")
-    .Attr("detector_spacing : tensor")
-    .Attr("ray_vectors : tensor")
+    .Input("volume_shape: int32")
+    .Input("volume_origin : float")
+    .Input("detector_origin : float")
+    .Input("volume_spacing : float")
+    .Input("detector_spacing : float")
+    .Input("ray_vectors : float")
     .Output("output: float")
     .SetShapeFn( []( ::tensorflow::shape_inference::InferenceContext* c )
     {
-      TensorShapeProto sp;
-      ShapeHandle sh;
-      ShapeHandle batch;
-      ShapeHandle out;
-      auto status = c->GetAttr( "volume_shape", &sp );
-      status.Update( c->MakeShapeFromShapeProto( sp, &sh ) );
-      c->Subshape(c->input(0),0,1,&batch);
-      c->Concatenate(batch, sh, &out);
-      c->set_output( 0, out );
-      return status;
+      c->set_output( 0, c->input(1));
+      return Status::OK();
     } )
     .Doc(R"doc(
 Computes the 2D parallel backprojection of the input sinogram based on the given ray vectors
@@ -55,95 +45,72 @@ output: A Tensor.
 )doc");
 
 void Parallel_Backprojection2D_Kernel_Launcher(const float *sinogram_ptr, float *out, const float *ray_vectors, const int number_of_projections,
-                                               const int volume_width, const int volume_height, const float volume_spacing_x, const float volume_spacing_y,
-                                               const float volume_origin_x, const float volume_origin_y,
-                                               const int detector_size, const float detector_spacing, const float detector_origin);
+                                               const int volume_size_x, const int volume_size_y,
+                                               const float *volume_spacing_ptr, const float *volume_origin_ptr,
+                                               const int detector_size, const float *detector_spacing, const float *detector_origin);
 
 class ParallelBackprojection2DOp : public OpKernel
 {
-    TensorShape volume_shape;
-    int volume_width, volume_height;
-
-    TensorShape projection_shape;
-    int detector_size, number_of_projections;
-
-    float volume_origin_x, volume_origin_y;
-
-    float detector_origin;
-
-    float volume_spacing_x, volume_spacing_y;
-
-    float detector_spacing;
-
-    Eigen::Tensor<float, 2, Eigen::RowMajor> ray_vectors_;
-
   public:
-    explicit ParallelBackprojection2DOp(OpKernelConstruction *context) : OpKernel(context)
-    {
-
-        //get volume shape from attributes
-        OP_REQUIRES_OK(context, context->GetAttr("sinogram_shape", &projection_shape));
-        number_of_projections = projection_shape.dim_size(0);
-        detector_size = projection_shape.dim_size(1);
-        //get detector shape from attributes
-        OP_REQUIRES_OK(context, context->GetAttr("volume_shape", &volume_shape));
-        volume_height = volume_shape.dim_size(0);
-        volume_width = volume_shape.dim_size(1);
-        //get volume origin from attributes
-        Tensor volume_origin_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("volume_origin", &volume_origin_tensor));
-        auto volume_origin_eigen = volume_origin_tensor.tensor<float, 1>();
-        volume_origin_y = volume_origin_eigen(0);
-        volume_origin_x = volume_origin_eigen(1);
-        //get detector origin from attributes
-        Tensor detector_origin_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("detector_origin", &detector_origin_tensor));
-        auto detector_origin_eigen = detector_origin_tensor.tensor<float, 1>();
-        detector_origin = detector_origin_eigen(0);
-
-        //get volume spacing from attributes
-        Tensor volume_spacing_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("volume_spacing", &volume_spacing_tensor));
-        auto volume_spacing_eigen = volume_spacing_tensor.tensor<float, 1>();
-        volume_spacing_y = volume_spacing_eigen(0);
-        volume_spacing_x = volume_spacing_eigen(1);
-
-        //get detector origin from attributes
-        Tensor detector_spacing_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("detector_spacing", &detector_spacing_tensor));
-        auto detector_spacing_eigen = detector_spacing_tensor.tensor<float, 1>();
-        detector_spacing = detector_spacing_eigen(0);
-
-        //get rey vectors from attributes
-        Tensor ray_vectors_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("ray_vectors", &ray_vectors_tensor));
-        auto ray_vectors_eigen = ray_vectors_tensor.tensor<float, 2>();
-        ray_vectors_ = Eigen::Tensor<float, 2, Eigen::RowMajor>(ray_vectors_eigen);
-    }
+    explicit ParallelBackprojection2DOp(OpKernelConstruction *context) : OpKernel(context){ }
 
     void Compute(OpKernelContext *context) override
     {
         // Grab the input tensor
         const Tensor &input_tensor = context->input(0);
-        auto input = input_tensor.flat<float>();
+        auto input = input_tensor.flat_outer_dims<float>();
+        TensorShape input_shape = input_tensor.shape();
+        int batch_size = input_tensor.shape().dim_size(0);
 
+        // Grab the volume_shape Tensor. Assuming input Tensor with [batch, number_of_projections, detector_width]
+        const Tensor &input_volume_shape = context->input(1);
+        auto volume_shape = input_volume_shape.flat_outer_dims<int>();
+
+        // Grab the volume_origin Tensor. 
+        const Tensor &input_volume_origin = context->input(2);
+        auto volume_origin = input_volume_origin.flat_outer_dims<float>();
+       
+        // Grab the detector origin Tensor.  
+        const Tensor &input_detector_origin = context->input(3);
+        auto detector_origin = input_detector_origin.flat_outer_dims<float>();
+
+        // Grab the volumen_spacing Tensor.  
+        const Tensor &input_volume_spacing = context->input(4);
+        auto volume_spacing = input_volume_spacing.flat_outer_dims<float>();
+        
+        // Grab the detector_spacing Tensor.  
+        const Tensor &input_detector_spacing = context->input(5);
+        auto detector_spacing = input_detector_spacing.flat_outer_dims<float>();
+        
+        // Grab the ray_vector Tensor.  Assuming input Tensor with [batch, ray_vectors]
+        const Tensor &input_ray_vector = context->input(6);
+        auto ray_vectors = input_ray_vector.flat_outer_dims<float>();
+        TensorShape ray_vector_shape = input_ray_vector.shape();
+
+        // Copy information on output shape to host memory.
+        int volume_shape_host_ptr[2];
+        auto code = cudaMemcpy(&volume_shape_host_ptr[0], input_volume_shape.flat<int>().data(), 2*sizeof(int), cudaMemcpyDeviceToHost);
+        int volume_x = volume_shape_host_ptr[ 1 ];
+        int volume_y = volume_shape_host_ptr[ 0 ];
         // Create output shape: [batch_size, volume_shape] 
         TensorShape out_shape = TensorShape(
-          {input_tensor.shape().dim_size(0), volume_shape.dim_size(0), volume_shape.dim_size(1)});
+          {batch_size, volume_y, volume_x});
         //Check supported batch size. Batch > 1 is not supported currently.
-        OP_REQUIRES(context, input_tensor.shape().dim_size(0) == 1,
-                errors::InvalidArgument("Batch dimension is mandatory ! Batch size > 1 is not supported in the current PYRO-NN-layers."));
+        // OP_REQUIRES(context, input_tensor.shape().dim_size(0) == 1,
+        //         errors::InvalidArgument("Batch dimension is mandatory ! Batch size > 1 is not supported in the current PYRO-NN-layers."));
 
         // Create an output tensor
         Tensor *output_tensor = nullptr;
         OP_REQUIRES_OK(context, context->allocate_output(0, out_shape,
                                                          &output_tensor));
-        auto output = output_tensor->template flat<float>();
-
+        auto output = output_tensor->flat_outer_dims<float>();
+        int index = 0;
         // Call the cuda kernel launcher
-        Parallel_Backprojection2D_Kernel_Launcher(input.data(), output.data(), ray_vectors_.data(), number_of_projections,
-                                                  volume_width, volume_height, volume_spacing_x, volume_spacing_y, volume_origin_x, volume_origin_y,
-                                                  detector_size, detector_spacing, detector_origin);
+        for(int index = 0; index < batch_size; ++index){
+          Parallel_Backprojection2D_Kernel_Launcher(&input(index,0), &output(index,0), &ray_vectors(index,0), input_shape.dim_size(1),
+                                                    volume_x, volume_y, &volume_spacing(index,0), &volume_origin(index,0),
+                                                    input_shape.dim_size(2), &detector_spacing(index,0), &detector_origin(index,0));
+        }
     }
 };
 
