c63fe38515ed5a7030df65f9b2515a21d2ffb996
syben <christopher.syben@fau.de>
2020-03-28 18:39:52 +0100

    minor adjustments

--- cc/helper_headers/helper_eigen.h
+++ cc/helper_headers/helper_eigen.h
@@ -22,13 +22,13 @@ template<typename T>
 using  MatrixType = Eigen::Matrix<T,Eigen::Dynamic, Eigen::Dynamic>;
 
 template<typename Scalar,int rank, typename sizeType>
-MatrixType<Scalar> Tensor_to_Matrix(const Eigen::Tensor<Scalar,rank> &tensor,const sizeType rows,const sizeType cols)
+__host__ __device__ MatrixType<Scalar> Tensor_to_Matrix(const Eigen::Tensor<Scalar,rank> &tensor,const sizeType rows,const sizeType cols)
 {
     return Eigen::Map<const MatrixType<Scalar>> (tensor.data(), rows,cols);
 }
 
 template<typename Scalar, typename... Dims>
-Eigen::Tensor< Scalar, sizeof... (Dims)> Matrix_to_Tensor(const MatrixType<Scalar> &matrix, Dims... dims)
+__host__ __device__ Eigen::Tensor< Scalar, sizeof... (Dims)> Matrix_to_Tensor(const MatrixType<Scalar> &matrix, Dims... dims)
 {
     constexpr int rank = sizeof... (Dims);
     return Eigen::TensorMap<Eigen::Tensor<const Scalar, rank>>(matrix.data(), {dims...});
