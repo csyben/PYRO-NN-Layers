9f7f4440017041f331d3a0120addbd7b5230426f
syben <christopher.syben@fau.de>
2020-03-29 16:49:12 +0200

    remove old comments

--- cc/ops/fan_backprojector_2D_OPKernel.cc
+++ cc/ops/fan_backprojector_2D_OPKernel.cc
@@ -26,28 +26,19 @@ using shape_inference::ShapeHandle;
 
 REGISTER_OP(CUDA_OPERATOR_KERNEL)
     .Input("sinogram: float")
-    .Attr("sinogram_shape: shape")
-    .Attr("volume_shape: shape")
-    .Attr("volume_origin : tensor")
-    .Attr("detector_origin : tensor")
-    .Attr("volume_spacing : tensor")
-    .Attr("detector_spacing : tensor")
-    .Attr("source_2_isocenter_distance : float")
-    .Attr("source_2_detector_distance : float")
-    .Attr("central_ray_vectors : tensor")
+    .Input("volume_shape: int32")
+    .Input("volume_origin : float")
+    .Input("detector_origin : float")
+    .Input("volume_spacing : float")
+    .Input("detector_spacing : float")
+    .Input("source_2_isocenter_distance : float")
+    .Input("source_2_detector_distance : float")
+    .Input("central_ray_vectors : float")
     .Output("output: float")
     .SetShapeFn( []( ::tensorflow::shape_inference::InferenceContext* c )
     {
-      TensorShapeProto sp;
-      ShapeHandle sh;
-      ShapeHandle batch;
-      ShapeHandle out;
-      auto status = c->GetAttr( "volume_shape", &sp );
-      status.Update( c->MakeShapeFromShapeProto( sp, &sh ) );
-      c->Subshape(c->input(0),0,1,&batch);
-      c->Concatenate(batch, sh, &out);
-      c->set_output( 0, out );
-      return status;
+      c->set_output( 0, c->input(1));
+      return Status::OK();
     } )
     .Doc(R"doc(
 Computes the 2D parallel backprojection of the input sinogram based on the given ray vectors
@@ -57,101 +48,79 @@ output: A Tensor.
 )doc");
 
 void Fan_Backprojection2D_Kernel_Launcher(const float *sinogram_ptr, float *out, const float *ray_vectors, const int number_of_projections,
-                                          const int volume_width, const int volume_height, const float volume_spacing_x, const float volume_spacing_y,
-                                          const float volume_origin_x, const float volume_origin_y,
-                                          const int detector_size, const float detector_spacing, const float detector_origin,
-                                          const float sid, const float sdd);
+                                          const int volume_size_x, const int volume_size_y, const float *volume_spacing, const float *volume_origin,
+                                          const int detector_size, const float *detector_spacing, const float *detector_origin,
+                                          const float *sid, const float *sdd);
 
 class FanBackprojection2DOp : public OpKernel
 {
-    TensorShape volume_shape;
-    int volume_width, volume_height;
-
-    TensorShape projection_shape;
-    int detector_size, number_of_projections;
-
-    float volume_origin_x, volume_origin_y;
-
-    float detector_origin;
-
-    float volume_spacing_x, volume_spacing_y;
-
-    float detector_spacing;
-
-    float sid, sdd;
-
-    Eigen::Tensor<float, 2, Eigen::RowMajor> ray_vectors_;
-
   public:
     explicit FanBackprojection2DOp(OpKernelConstruction *context) : OpKernel(context)
     {
-
-        //get volume shape from attributes
-        OP_REQUIRES_OK(context, context->GetAttr("sinogram_shape", &projection_shape));
-        number_of_projections = projection_shape.dim_size(0);
-        detector_size = projection_shape.dim_size(1);
-        //get detector shape from attributes
-        OP_REQUIRES_OK(context, context->GetAttr("volume_shape", &volume_shape));
-        volume_height = volume_shape.dim_size(0);
-        volume_width = volume_shape.dim_size(1);
-        //get volume origin from attributes
-        Tensor volume_origin_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("volume_origin", &volume_origin_tensor));
-        auto volume_origin_eigen = volume_origin_tensor.tensor<float, 1>();
-        volume_origin_y = volume_origin_eigen(0);
-        volume_origin_x = volume_origin_eigen(1);
-        //get detector origin from attributes
-        Tensor detector_origin_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("detector_origin", &detector_origin_tensor));
-        auto detector_origin_eigen = detector_origin_tensor.tensor<float, 1>();
-        detector_origin = detector_origin_eigen(0);
-
-        //get volume spacing from attributes
-        Tensor volume_spacing_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("volume_spacing", &volume_spacing_tensor));
-        auto volume_spacing_eigen = volume_spacing_tensor.tensor<float, 1>();
-        volume_spacing_y = volume_spacing_eigen(0);
-        volume_spacing_x = volume_spacing_eigen(1);
-
-        //get detector origin from attributes
-        Tensor detector_spacing_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("detector_spacing", &detector_spacing_tensor));
-        auto detector_spacing_eigen = detector_spacing_tensor.tensor<float, 1>();
-        detector_spacing = detector_spacing_eigen(0);
-
-        OP_REQUIRES_OK(context, context->GetAttr("source_2_isocenter_distance", &sid));
-        OP_REQUIRES_OK(context, context->GetAttr("source_2_detector_distance", &sdd));
-
-        //get rey vectors from attributes
-        Tensor ray_vectors_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("central_ray_vectors", &ray_vectors_tensor));
-        auto ray_vectors_eigen = ray_vectors_tensor.tensor<float, 2>();
-        ray_vectors_ = Eigen::Tensor<float, 2, Eigen::RowMajor>(ray_vectors_eigen);
     }
 
     void Compute(OpKernelContext *context) override
     {
         // Grab the input tensor
         const Tensor &input_tensor = context->input(0);
-        auto input = input_tensor.flat<float>();
+        auto input = input_tensor.flat_outer_dims<float>();
+        TensorShape input_shape = input_tensor.shape();
+        int batch_size = input_tensor.shape().dim_size(0);
 
+        // Grab the volume_shape Tensor. Assuming input Tensor with [batch, number_of_projections, detector_width]
+        const Tensor &input_volume_shape = context->input(1);
+        auto volume_shape = input_volume_shape.flat_outer_dims<int>();
+
+        // Grab the volume_origin Tensor. 
+        const Tensor &input_volume_origin = context->input(2);
+        auto volume_origin = input_volume_origin.flat_outer_dims<float>();
+       
+        // Grab the detector origin Tensor.  
+        const Tensor &input_detector_origin = context->input(3);
+        auto detector_origin = input_detector_origin.flat_outer_dims<float>();
+
+        // Grab the volumen_spacing Tensor.  
+        const Tensor &input_volume_spacing = context->input(4);
+        auto volume_spacing = input_volume_spacing.flat_outer_dims<float>();
+        
+        // Grab the detector_spacing Tensor.  
+        const Tensor &input_detector_spacing = context->input(5);
+        auto detector_spacing = input_detector_spacing.flat_outer_dims<float>();
+        
+        // Grab the detector_spacing Tensor.  
+        const Tensor &input_source_isocenter_distance = context->input(6);
+        auto sid = input_source_isocenter_distance.flat_outer_dims<float>();
+        
+        // Grab the detector_spacing Tensor.  
+        const Tensor &input_source_detector_distance = context->input(7);
+        auto sdd = input_source_detector_distance.flat_outer_dims<float>();
+
+        // Grab the ray_vector Tensor.  Assuming input Tensor with [batch, ray_vectors]
+        const Tensor &input_ray_vector = context->input(8);
+        auto ray_vectors = input_ray_vector.flat_outer_dims<float>();
+        TensorShape ray_vector_shape = input_ray_vector.shape();
+
+        // Copy information on output shape to host memory.
+        int volume_shape_host_ptr[2];
+        auto code = cudaMemcpy(&volume_shape_host_ptr[0], input_volume_shape.flat<int>().data(), 2*sizeof(int), cudaMemcpyDeviceToHost);
+        int volume_x = volume_shape_host_ptr[ 1 ];
+        int volume_y = volume_shape_host_ptr[ 0 ];
         // Create output shape: [batch_size, volume_shape] 
         TensorShape out_shape = TensorShape(
-          {input_tensor.shape().dim_size(0), volume_shape.dim_size(0), volume_shape.dim_size(1)});
-        //Check supported batch size. Batch > 1 is not supported currently.
-        OP_REQUIRES(context, input_tensor.shape().dim_size(0) == 1,
-                errors::InvalidArgument("Batch dimension is mandatory ! Batch size > 1 is not supported in the current PYRO-NN-layers."));
-
+          {batch_size, volume_y, volume_x});
         // Create an output tensor
         Tensor *output_tensor = nullptr;
         OP_REQUIRES_OK(context, context->allocate_output(0, out_shape,
                                                          &output_tensor));
-        auto output = output_tensor->template flat<float>();
+        auto output = output_tensor->flat_outer_dims<float>();
 
         // Call the cuda kernel launcher
-        Fan_Backprojection2D_Kernel_Launcher(input.data(), output.data(), ray_vectors_.data(), number_of_projections,
-                                             volume_width, volume_height, volume_spacing_x, volume_spacing_y, volume_origin_x, volume_origin_y,
-                                             detector_size, detector_spacing, detector_origin, sid, sdd);
+        for(int index = 0; index < batch_size; ++index){
+          Fan_Backprojection2D_Kernel_Launcher(&input(index,0), &output(index,0), &ray_vectors(index,0), input_shape.dim_size(1),
+                                              volume_x, volume_y, &volume_spacing(index,0), &volume_origin(index,0),
+                                               input_shape.dim_size(2), &detector_spacing(index,0), &detector_origin(index,0),
+                                               &sid(index,0), &sdd(index,0));
+        }
     }
 };
 
