39a3854c247cb806ca8833c90b63ba266f0b6666
syben <christopher.syben@fau.de>
2020-03-29 16:48:16 +0200

    Change Layer API to use mostly INPUT for TF2.0 and eager excecution. Added batch size support

--- cc/ops/cone_backprojector_3D_OPKernel.cc
+++ cc/ops/cone_backprojector_3D_OPKernel.cc
@@ -27,27 +27,18 @@ using shape_inference::ShapeHandle;
 
 REGISTER_OP(CUDA_OPERATOR_KERNEL)
     .Input("sinogram: float")
-    .Attr("sinogram_shape: shape")
-    .Attr("volume_shape: shape")
-    .Attr("volume_origin : tensor")
-    .Attr("volume_spacing : tensor")
-    .Attr("projection_multiplier : float")
-    .Attr("projection_matrices : tensor")
+    .Input("volume_shape: int32")
+    .Input("volume_origin : float")
+    .Input("volume_spacing : float")
+    .Input("projection_matrices : float")
+    .Input("step_size : float")
+    .Input("projection_multiplier : float")
     .Attr("hardware_interp : bool = false")
-    .Attr("step_size : float")
     .Output("output: float")
     .SetShapeFn( []( ::tensorflow::shape_inference::InferenceContext* c )
     {
-      TensorShapeProto sp;
-      ShapeHandle sh;
-      ShapeHandle batch;
-      ShapeHandle out;
-      auto status = c->GetAttr( "volume_shape", &sp );
-      status.Update( c->MakeShapeFromShapeProto( sp, &sh ) );
-      c->Subshape(c->input(0),0,1,&batch);
-      c->Concatenate(batch, sh, &out);
-      c->set_output( 0, out );
-      return status;
+      c->set_output( 0, c->input(1));
+      return Status::OK();
     } )
     .Doc(R"doc(
 Computes the 3D cone backprojection of the input sinogram on the given the trajectory
@@ -58,129 +49,89 @@ output: A Tensor.
 
 void Cone_Backprojection3D_Kernel_Tex_Interp_Launcher(const float *sinogram_ptr, float *out, const float *projection_matrix, const int number_of_projections,
                                     const int volume_width, const int volume_height, const int volume_depth, 
-                                    const float volume_spacing_x, const float volume_spacing_y, const float volume_spacing_z,
-                                    const float volume_origin_x, const float volume_origin_y, const float volume_origin_z,
-                                    const int detector_width, const int detector_height, const float projection_multiplier);
+                                    const float *volume_spacing, const float *volume_origin,
+                                    const int detector_width, const int detector_height, const float *projection_multiplier);
 
 void Cone_Backprojection3D_Kernel_Launcher(const float *sinogram_ptr, float *out, const float *projection_matrix, const int number_of_projections,
                                     const int volume_width, const int volume_height, const int volume_depth, 
-                                    const float volume_spacing_x, const float volume_spacing_y, const float volume_spacing_z,
-                                    const float volume_origin_x, const float volume_origin_y, const float volume_origin_z,
-                                    const int detector_width, const int detector_height, const float projection_multiplier);
+                                    const float *volume_spacing, const float *volume_origin,
+                                    const int detector_width, const int detector_height, const float *projection_multiplier);
 
 class ConeBackprojection3DOp : public OpKernel
 {
-    TensorShape volume_shape;
-    int volume_width, volume_height, volume_depth;
-
-    TensorShape projection_shape;
-    int detector_size_x, detector_size_y, number_of_projections;
-
-    float volume_origin_x, volume_origin_y, volume_origin_z;
-
-    float volume_spacing_x, volume_spacing_y, volume_spacing_z;
-
-    float projection_multiplier;
-
-    Eigen::Tensor<float, 3, Eigen::RowMajor> projection_matrices;
-
     bool hardware_interp;
-    float step_size;
 
   public:
     explicit ConeBackprojection3DOp(OpKernelConstruction *context) : OpKernel(context)
-    {        
-        //get detector shape from attributes
-        OP_REQUIRES_OK(context, context->GetAttr("sinogram_shape", &projection_shape));
-        number_of_projections = projection_shape.dim_size(0);
-        detector_size_y = projection_shape.dim_size(1);
-        detector_size_x = projection_shape.dim_size(2);
-        //get volume shape from attributes
-        OP_REQUIRES_OK(context, context->GetAttr("volume_shape", &volume_shape));
-        volume_depth = volume_shape.dim_size(0);
-        volume_height = volume_shape.dim_size(1);
-        volume_width = volume_shape.dim_size(2);
-        //get volume origin from attributes
-        Tensor volume_origin_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("volume_origin", &volume_origin_tensor));
-        auto volume_origin_eigen = volume_origin_tensor.tensor<float, 1>();
-        volume_origin_z = volume_origin_eigen(0);
-        volume_origin_y = volume_origin_eigen(1);
-        volume_origin_x = volume_origin_eigen(2);
-
-        //get volume spacing from attributes
-        Tensor volume_spacing_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("volume_spacing", &volume_spacing_tensor));
-        auto volume_spacing_eigen = volume_spacing_tensor.tensor<float, 1>();
-        volume_spacing_z = volume_spacing_eigen(0);
-        volume_spacing_y = volume_spacing_eigen(1);
-        volume_spacing_x = volume_spacing_eigen(2);
-
-        //get ray vectors from attributes
-        Tensor projection_matrices_tensor;
-        OP_REQUIRES_OK(context, context->GetAttr("projection_matrices", &projection_matrices_tensor));
-        auto projection_matrices_eigen = projection_matrices_tensor.tensor<float, 3>();
-        projection_matrices = Eigen::Tensor<float, 3, Eigen::RowMajor>( number_of_projections, 3, 4 );
-
-        //get hardware interpolation flag
-        OP_REQUIRES_OK(context, context->GetAttr("hardware_interp", &hardware_interp));
-
-        //get discretization invariant and constant part of distance
-        OP_REQUIRES_OK(context, context->GetAttr("projection_multiplier", &projection_multiplier));
-
-        //set_size for projection operator as gradient
-        OP_REQUIRES_OK(context, context->GetAttr("step_size", &step_size));
-
-        //TODO: Loop is not neseccary anymore, refactor to direct conversion
-        //for each projection
-        for (int n = 0; n < number_of_projections; n++)
-        {   
-            // loop over matrix
-            for( int i = 0; i < 3; ++i )
-            {
-                for( int j = 0; j < 4; ++j )
-                {
-                    projection_matrices(n,i,j) = projection_matrices_eigen(n,i,j);
-                }
-            }
-        }
+    {     
+        OP_REQUIRES_OK(context, context->GetAttr("hardware_interp", &hardware_interp));   
     }
 
     void Compute(OpKernelContext *context) override
     {
-        // Grab the input tensor
+                // Grab the input Tensor (Volume).  Assuming input Tensor with [batch, height, width]
         const Tensor &input_tensor = context->input(0);
-        auto input = input_tensor.flat<float>();        
+        auto input = input_tensor.flat_outer_dims<float>();
+        TensorShape input_shape = input_tensor.shape();
+        int batch_size = input_tensor.shape().dim_size(0);
+
+        // Grab the projection_shape Tensor. Assuming input Tensor with [batch, number_of_projections, detector_heigt, detector_width]
+        const Tensor &input_volume_shape = context->input(1);
+        auto volume_shape = input_volume_shape.flat_outer_dims<int>();
+
+        // Grab the volume_origin Tensor. 
+        const Tensor &input_volume_origin = context->input(2);
+        auto volume_origin = input_volume_origin.flat_outer_dims<float>();
+
+        // Grab the volumen_spacing Tensor.  
+        const Tensor &input_volume_spacing = context->input(3);
+        auto volume_spacing = input_volume_spacing.flat_outer_dims<float>();
+        
+        // Grab the ray_vector Tensor.  Assuming input Tensor with [batch, ray_vectors]
+        const Tensor &input_proj_matrices = context->input(4);
+        auto proj_matrices = input_proj_matrices.flat_outer_dims<float>();
+        TensorShape proj_matrices_shape = input_proj_matrices.shape();
+
+        // Grab the detector_spacing Tensor.  
+        const Tensor &input_step_size = context->input(5);
+        auto step_size = input_step_size.flat_outer_dims<float>();
+
+        // Grab the detector_spacing Tensor.  
+        const Tensor &input_projection_multiplier = context->input(6);
+        auto proj_multiplier = input_projection_multiplier.flat_outer_dims<float>();
+           
+        // Copy information on output shape to host memory.
+        // Implicit assumption that the input shape is consistent over the batch !
+        int volume_shape_host_ptr[3];
+        auto code = cudaMemcpy(&volume_shape_host_ptr[0], input_volume_shape.flat<int>().data(), 3*sizeof(int), cudaMemcpyDeviceToHost);
+        int volume_x =  volume_shape_host_ptr[ 2 ];
+        int volume_y =  volume_shape_host_ptr[ 1 ];
+        int volume_z = volume_shape_host_ptr[ 0 ];
+        // Create output shape: [batch_size, volume_shape] 
+        TensorShape out_shape = TensorShape(
+          {batch_size, volume_z,volume_y, volume_x});
         // Create an output tensor
         Tensor *output_tensor = nullptr;
-        
-        TensorShape out_shape = TensorShape(
-            {input_tensor.shape().dim_size(0), volume_shape.dim_size(0), volume_shape.dim_size(1), volume_shape.dim_size(2)});
-
-        // Check Batch size. Batch > 1 is not supported currently.
-        OP_REQUIRES(context, input_tensor.shape().dim_size(0)  == 1,
-                errors::InvalidArgument("Batch dimension is mandatory ! Batch size > 1 is not supported in the current PYRO-NN-layers."));
-
         OP_REQUIRES_OK(context, context->allocate_output(0, out_shape,
                                                          &output_tensor));
-        auto output = output_tensor->template flat<float>();
+        auto output = output_tensor->flat_outer_dims<float>();
 
-        // Call the cuda kernel launcherst
-        if(hardware_interp)
-        {
-            Cone_Backprojection3D_Kernel_Tex_Interp_Launcher(input.data(), output.data(), projection_matrices.data(), number_of_projections,
-                                        volume_width, volume_height, volume_depth, volume_spacing_x, volume_spacing_y, volume_spacing_z,
-                                        volume_origin_x, volume_origin_y, volume_origin_z,
-                                        detector_size_x, detector_size_y,
-                                        projection_multiplier);
-        }
-        else
-        {
-            Cone_Backprojection3D_Kernel_Launcher(input.data(), output.data(), projection_matrices.data(), number_of_projections,
-                                        volume_width, volume_height, volume_depth, volume_spacing_x, volume_spacing_y, volume_spacing_z,
-                                        volume_origin_x, volume_origin_y, volume_origin_z,
-                                        detector_size_x, detector_size_y,
-                                        projection_multiplier);
+        // Call the cuda kernel launcher
+        for(int index = 0; index < batch_size; ++index){
+            if(hardware_interp)
+            {
+                Cone_Backprojection3D_Kernel_Tex_Interp_Launcher(&input(index,0), &output(index,0),&proj_matrices(index,0),
+                                            input_shape.dim_size(1),volume_x, volume_y, volume_z, &volume_spacing(index,0),
+                                            &volume_origin(index,0), input_shape.dim_size(3), input_shape.dim_size(2),
+                                            &proj_multiplier(index,0));
+            }
+            else
+            {
+                Cone_Backprojection3D_Kernel_Launcher(&input(index,0), &output(index,0),&proj_matrices(index,0),
+                                            input_shape.dim_size(1),volume_x, volume_y, volume_z, &volume_spacing(index,0),
+                                            &volume_origin(index,0), input_shape.dim_size(3), input_shape.dim_size(2),
+                                            &proj_multiplier(index,0));
+            }
         }
     }
 };
